8ffde4d1bb6222f74122b5461c884337caecbf2e
PoldervaartD <dennispoldervaart@hotmail.nl>
2022-09-22 11:34:00 +0200

    test game

---
+++ Assets/Scripts/eNDMENU.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eNDMENU : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
